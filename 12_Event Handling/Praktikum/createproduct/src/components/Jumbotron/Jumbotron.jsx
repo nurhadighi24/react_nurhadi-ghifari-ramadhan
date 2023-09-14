f1579d7aeebc63fc6f048427d6a3a1e307e308df
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2023-09-14 23:40:29 +0700

    Penambahan fitur button ganti bahasa pada soal eksplorasi

--- 12_Event Handling/Praktikum/createproduct/src/components/Jumbotron/Jumbotron.jsx
+++ 12_Event Handling/Praktikum/createproduct/src/components/Jumbotron/Jumbotron.jsx
@@ -4,11 +4,11 @@ import JumbotronTitle from "./JumbotronTitle";
 import JumbotronBody from "./JumbotronBody";
 import { article } from "../../utils/article";
 
-export default function Jumbotron() {
+export default function Jumbotron({ currentLanguage }) {
   return (
     <div className="text-center mt-5">
-      <JumbotronTitle title={article.title.en} />
-      <JumbotronBody body={article.description.en} />
+      <JumbotronTitle title={article.title[currentLanguage]} />
+      <JumbotronBody body={article.description[currentLanguage]} />
     </div>
   );
 }

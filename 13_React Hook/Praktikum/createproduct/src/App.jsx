06debe02f6688d115f429f5ce2ad8336f4169814
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2023-09-14 23:35:00 +0700

    Pengumpulan Latihan - React Hook

---
+++ 13_React Hook/Praktikum/createproduct/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+import Header from "./components/Header/Header";
+import Button from "./components/Button/Button";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Form from "./components/Form/Form";
+
+import "../src/styles/App.css";
+import RandomNumber from "./utils/RandomNumber";
+
+function App() {
+  const [currentLanguage, setCurrentLanguage] = useState("en");
+
+  useEffect(() => {
+    window.alert("Welcome");
+  }, []);
+
+  const changeLanguage = () => {
+    setCurrentLanguage(currentLanguage === "en" ? "id" : "en");
+  };
+
+  return (
+    <>
+      <Header />
+      <Jumbotron currentLanguage={currentLanguage} />
+      <Form />
+      <Button
+        label="klik untuk ganti bahasa di jumbotron"
+        onClick={changeLanguage}
+      />
+      <Button
+        label="Klik untuk mendapatkan random number di console"
+        onClick={RandomNumber}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ 13_React Hook/Praktikum/createproduct/src/components/Form/Input/Input.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Input({ label, onChange, type, value, defaultValue, placeholder }) {
+  return (
+    <div className="flex flex-col">
+      <label>{label}</label>
+      <input
+        className="border-2 rounded-md"
+        type={type}
+        onChange={onChange}
+        value={value}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
+
+function Select({ label, placeholder, value, onChange, options = [] }) {
+  return (
+    <div className="flex flex-col">
+      <label>{label}</label>
+      <select className="border-2 rounded-md" onChange={onChange} value={value}>
+        <option>{placeholder}</option>
+        {options.map((option) => (
+          <option key={option}>{option}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function RadioInput({ label, name, value, checked, onChange }) {
+  return (
+    <div>
+      <input
+        type="radio"
+        id={value}
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+      />
+      <label htmlFor={value}>{label}</label>
+    </div>
+  );
+}
+
+export { Input, Select, RadioInput };
